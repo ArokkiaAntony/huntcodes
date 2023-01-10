67bd0513d7aa8fd2451dca8b5b236567cc6dbdba
ArokkiaAntony <antonyvenkat96@gmail.com>
2023-01-10 21:51:23 +0530

    five method created

---
+++ src/emp/Pullrequest.java
@@ -0,0 +1,24 @@
+package emp;
+
+public class Pullrequest {
+	private void tc1() {
+		System.out.println("antony");
+	}
+	private void tc2() {
+			System.out.println("antony");
+	}
+	private void tc3() {
+				System.out.println("antony");
+	}
+	private void tc4() {
+					System.out.println("antony");
+	}
+	private void tc5() {
+						System.out.println("antony");
+	}
+	private void tc6() {
+		System.out.println("antony");
+
+	}
+
+}
